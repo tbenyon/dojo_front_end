43da37dc2d16b87c8bf29dd852972fc4b2436614
Tom Benyon <Tom.Benyon@creative-assembly.com>
2016-09-14 16:56:04 +0100

    Add additional security
    httpOnly: true => don't let browser js access cookies
    secure: true => only user cookies over https
    ephemeral: true => delete session cookie when the browser is closed

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(session({
     cookieName: 'session',
     secret: 'random_string_goes_here',
     duration: 30 * 60 * 1000,
-    activeDuration: 5 * 60 * 1000
+    activeDuration: 5 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    ephemeral: true
 }));
 app.use(csrf());
 
