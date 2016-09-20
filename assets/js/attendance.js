09fb7690545d5eceff81fedbd22f027f72d2a680
Tom Benyon <Tom.Benyon@creative-assembly.com>
2016-09-20 15:47:45 +0100

    Remove duplication

--- assets/js/attendance.js
+++ assets/js/attendance.js
@@ -1,52 +1,33 @@
 var ctx = document.getElementById("attendanceChart").getContext("2d");
 
+var DataSet = function (label, data, lineColour) {
+    this.label = label;
+    this.fill = false;
+    this.lineTension = 0.1;
+    this.backgroundColor = "rgba(" + lineColour + ",0.4)";
+    this.borderColor = "rgba(" + lineColour + ",1)";
+    this.borderCapStyle = 'butt';
+    this.borderDash = [];
+    this.borderDashOffset = 0.0;
+    this.borderJoinStyle = 'miter';
+    this.pointBorderColor = "rgba(" + lineColour + ",1)";
+    this.pointBackgroundColor = "#fff";
+    this.pointBorderWidth = 1;
+    this.pointHoverRadius = 5;
+    this.pointHoverBackgroundColor = "rgba(" + lineColour + ",1)";
+    this.pointHoverBorderColor = "rgba(220,220,220,1)";
+    this.pointHoverBorderWidth = 2;
+    this.pointRadius = 1;
+    this.pointHitRadius = 10;
+    this.data = data;
+    this.spanGaps = false;
+};
+
 var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
-        {
-            label: "Mentors",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 60, 60, 56, 55, 40],
-            spanGaps: false
-        },
-        {
-            label: "Students",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(255,192,192,0.4)",
-            borderColor: "rgba(192,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [34, 59, 23, 12, 34, 2, 34],
-            spanGaps: false
-        }
+        new DataSet("Mentors", [1,9,3,3,5,23,4], "255,50,50"),
+        new DataSet("Students", [12,5,1,3,5,3,4], "75,255,100")
     ]
 };
 
