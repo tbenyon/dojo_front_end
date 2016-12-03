d9f10191575df025841f57600eb838c0ac106345
Tom Benyon <Tom.Benyon@creative-assembly.com>
2016-12-03 17:12:10 +0000

    Make multiple snowflakes fall

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -1,18 +1,28 @@
 var imgObj = null;
 var animate ;
+var number_of_snowballs = 5;
+var imageObjects = [];
 
 function init(){
-    imgObj = document.getElementById('snowflake1');
-    imgObj.style.top = '-20px';
-    imgObj.style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
+    alert("starting init");
+    for (var i = 0; i < number_of_snowballs; i++) {
+        imgObj = document.getElementById('snowflake' + i);
+        imgObj.style.top = '-20px';
+        imgObj.style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
+        imageObjects.push(imgObj);
+        alert(i);
+    }
     moveSnow();
+
 }
 
 function moveSnow(){
-    imgObj.style.top = parseInt(imgObj.style.top) + 10 + 'px';
-    if (parseInt(imgObj.style.top) > Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
-        imgObj.style.top = '-20px';
-        imgObj.style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
+    for (var i = 0; i < number_of_snowballs; i++) {
+        imageObjects[i].style.top = parseInt(imageObjects[i].style.top) + 10 + 'px';
+        if (parseInt(imageObjects[i].style.top) > Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
+            imageObjects[i].style.top = '-20px';
+            imageObjects[i].style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
+        }
     }
     animate = setTimeout(moveSnow,20); // call moveRight in 20msec
 }
