90e39051464bbf0bf8b55778ca9a8c7e404315d4
Tom Benyon <Tom.Benyon@creative-assembly.com>
2016-12-03 18:00:34 +0000

    Add varying speed

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -1,7 +1,8 @@
 var imgObj = null;
 var animate ;
-var number_of_snowballs = 30;
+var number_of_snowballs = 100;
 var imageObjects = [];
+var snowSpeed = 3;
 
 function init(){
     for (var i = 0; i < number_of_snowballs; i++) {
@@ -10,20 +11,36 @@ function init(){
         imgObj.style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
         imageObjects.push(imgObj);
     }
+    console.log("init done!");
     moveSnow();
 
 }
 
 function moveSnow(){
     for (var i = 0; i < number_of_snowballs; i++) {
-        imageObjects[i].style.top = parseInt(imageObjects[i].style.top) + 4 + 'px';
+        var increase = getSpeed(i);
+        console.log(increase);
+        imageObjects[i].style.top = parseInt(imageObjects[i].style.top) + increase + 'px';
         if (parseInt(imageObjects[i].style.top) > Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
             imageObjects[i].style.top = '-20px';
             imageObjects[i].style.left = Math.floor((Math.random() * window.innerWidth) + 1) + 'px';
-            imageObjects[i].style['z-index'] = Math.floor((Math.random() * 4) + -2);
+            if (i < number_of_snowballs / 3) {
+                imageObjects[i].style['z-index'] = Math.floor((Math.random() * 6) ) - 2;
+            }
         }
     }
     animate = setTimeout(moveSnow,20); // call moveRight in 20msec
+}
+
+function getSpeed(i) {
+    if (i < number_of_snowballs / 3) {
+        return snowSpeed
+    } else if (i < number_of_snowballs / 3 * 2) {
+        return snowSpeed / 2
+    } else {
+        return snowSpeed / 3
+    }
+
 }
 
 function stop(){
