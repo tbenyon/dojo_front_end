2df4327d5b1f0b7f32032cb54777d5196f40fae9
Tom Benyon <Tom.Benyon@creative-assembly.com>
2016-12-04 20:17:44 +0000

    Make snow work with positive and negative snow direction

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -3,7 +3,8 @@ var animate ;
 var number_of_snowballs = 100;
 var imageObjects = [];
 var snowSpeed = 3;
-var wind = -1;
+var wind = 1;
+var increase;
 
 var snow_creation_padding;
 var snow_creation_width;
@@ -21,22 +22,13 @@ function init(){
 }
 
 function moveSnow(){
-    snow_creation_padding = window.innerHeight / snowSpeed * -wind;
-    snow_creation_width = window.innerWidth + snow_creation_padding;
-    if (wind < 0) {
-        snow_creation_location = Math.floor((Math.random() * (   snow_creation_width   )) + 1)
-    } else {
-        snow_creation_location = Math.floor((Math.random() * (   snow_creation_width   )) + 1) - snow_creation_padding;
-    }
-
     for (var i = 0; i < number_of_snowballs; i++) {
-        var increase = getSpeed(i);
+        increase = getSpeed(i);
         imageObjects[i].style.top = parseInt(imageObjects[i].style.top) + increase + 'px';
         imageObjects[i].style.left = parseInt(imageObjects[i].style.left) + wind + 'px';
         if (parseInt(imageObjects[i].style.top) > Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
             imageObjects[i].style.top = '-20px';
-            imageObjects[i].style.left =  snow_creation_location + 'px';
-            console.log(imageObjects[i].style.left = Math.floor((Math.random() * (window.innerWidth + snow_creation_padding)) + 1) + 'px');
+            imageObjects[i].style.left =  getSnowCreationLocation() + 'px';
             if (i < number_of_snowballs / 3) {
                 imageObjects[i].style['z-index'] = Math.floor((Math.random() * 6) ) - 2;
             }
@@ -55,6 +47,18 @@ function getSpeed(i) {
         return snowSpeed / 3
     }
 
+}
+
+function getSnowCreationLocation() {
+    snow_creation_padding = (window.innerHeight / snowSpeed) * Math.abs(wind);
+    snow_creation_width = window.innerWidth + snow_creation_padding;
+    if (wind < 0) {
+        snow_creation_location = Math.floor((Math.random() * (   snow_creation_width   )) + 1);
+    } else {
+        snow_creation_location = Math.floor((Math.random() * (   snow_creation_width   )) + 1) - snow_creation_padding;
+    }
+    console.log(snow_creation_location);
+    return snow_creation_location;
 }
 
 function stop(){
